74b0f213b556fe27cad4da101b563e56b9caf4db
Bálint Mátyás Farsang <balint.farsang@stud.uni-corvinus.hu>
2020-10-25 23:21:48 +0100

    Birth+Death

---
+++ nepesseg/nepesseg/Entities/BirthProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nepesseg.Entities
+{
+    public class BirthProbability
+    {
+        public int Kor { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double P { get; set; }
+
+    }
+}
---
+++ nepesseg/nepesseg/Entities/DeathProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nepesseg.Entities
+{
+    public class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Kor { get; set; }
+        public double P { get; set; }
+
+    }
+}
