93727d04ad7ea451ac039300fb01fa7d04dc1260
Bálint Mátyás Farsang <balint.farsang@stud.uni-corvinus.hu>
2020-10-25 23:12:49 +0100

    enumGender+Person

---
+++ nepesseg/nepesseg/Entities/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nepesseg.Entities
+{
+    public class Person
+    {
+        public int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+        public int NbrOfChildren { get; set; }
+        public bool IsAlive { get; set; }
+
+        public Person()
+        {
+            IsAlive = true;
+        }
+    }
+}
